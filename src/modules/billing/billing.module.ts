bd0b64707cc67feef9181fe5b68e6518fc37e49c
lovanbang999 <lovanbangbox9@gmail.com>
2026-03-26 16:34:53 +0700

    feat(notifications): revamp email system with premium Vietnamese templates

--- src/modules/billing/billing.module.ts
+++ src/modules/billing/billing.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule],
   controllers: [BillingController],
   providers: [BillingService],
   exports: [BillingService],
